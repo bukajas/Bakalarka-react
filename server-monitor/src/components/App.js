402c3e2be140b187f64d9dcdbcac2dd5aa4c039b
bukajas <jakubasszonyi@gmail.com>
2022-04-24 15:37:43 +0200

    24.04 mazani nepotrebnych souboru
    smazal jsem nepotrebne soubory

--- server-monitor/src/components/App.js
+++ server-monitor/src/components/App.js
@@ -15,7 +15,6 @@ import {
 import Template from "./Template.js"
 import {format, set} from 'date-fns'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import TempGraf from './TempGraf'
 export { CheckboxInt }
 
 
